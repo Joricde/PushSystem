8847c84f7a574bbe3c2fd5c8735d38fc449a57c9
YOU <joricde@outlook.com>
2022-03-23 04:36:25 +0800

    添加日志模块

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"awesomeProject/config"
+	"awesomeProject/middleware"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +10,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.TrustedPlatform = gin.PlatformCloudflare
+	r.Use(middleware.GinLogger(), middleware.GinRecovery(true))
 	err := r.Run(fmt.Sprintf(":%d", config.Conf.AppConfig.Port))
 	if err != nil {
 		panic(err)
